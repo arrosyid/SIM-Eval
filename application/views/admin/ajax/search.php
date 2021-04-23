19904764249610a3c427e99a541238f63b98fe53
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-23 21:52:10 +0700

    +starter view ajax search

---
+++ application/views/admin/ajax/search.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+  <div class="row mb-2">
+    <?php foreach ($resi as $r) : ?>
+      <!-- isi search -->
+      <!-- /.coloumn -->
+    <?php endforeach ?>
+  </div>
+  <!-- /.row -->
+</div>
+<!-- /.container fluid -->
