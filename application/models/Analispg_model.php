d9e66133494ca9d13d3d6eb68c38a3a4f0c8dc76
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-25 21:17:29 +0700

    +model analispg

---
+++ application/models/Analispg_model.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Analispg_model extends CI_Model
+{
+  // mengambil semua Analispg
+  public function getAllAnalispg()
+  {
+    return $this->db->get('tb_analis_soalpg');
+  }
+
+  // mengambil data Analispg berdasarkan tipe id
+  public function getAnalispgByType($id, $type)
+  {
+    // berdasarkan idAnalispg
+    if ($type == 'id_analispg') {
+      $this->db->select('tb_analis_soalpg.*, tb_soal.*, tb_dist_jwbpg.*')
+        ->from('tb_analis_soalpg')
+        ->where(['id_analispg' => $id])
+        ->join('tb_soal.id_soal = tb_analis_soalpg.id_soal, tb_analis_soalpg.id_pg = tb_dist_jwbpg.id_pg');
+      return $this->db->get();
+    }
+
+    // berdasarkan id_pg
+    if ($type == 'id_pg') {
+      $this->db->select('tb_analis_soalpg.*, tb_soal.*, tb_dist_jwbpg.*')
+        ->from('tb_analis_soalpg')
+        ->where(['id_pg' => $id])
+        ->join('tb_soal.id_soal = tb_analis_soalpg.id_soal, tb_analis_soalpg.id_pg = tb_dist_jwbpg.id_pg');
+      return $this->db->get();
+    }
+    // berdasarkan id_soal
+    if ($type == 'id_soal') {
+      $this->db->select('tb_analis_soalpg.*, tb_soal.*, tb_dist_jwbpg.*')
+        ->from('tb_analis_soalpg')
+        ->where(['id_soal' => $id])
+        ->join('tb_soal.id_soal = tb_analis_soalpg.id_soal, tb_analis_soalpg.id_pg = tb_dist_jwbpg.id_pg');
+      return $this->db->get();
+    }
+  }
+
+  // update Analispg dari id
+  public function upadateAnalispgById($idAnalispg, $data)
+  {
+    return $this->db->update('tb_analis_soalpg', $data, ['id_analispg' => $idAnalispg]);
+  }
+
+  // delete Analispg
+  public function deleteAnalispgByType($id, $type)
+  {
+    // berdasarkan id pilhan ganda
+    if ($type == 'id_analispg')
+      return $this->db->delete('tb_analis_soalpg', ['id_analispg' => $id]);
+
+    // bersasarkan id pilihan ganda
+    if ($type == 'id_pg')
+      return $this->db->delete('tb_analis_soalpg', ['id_pg' => $id]);
+
+    // bersasarkan id soal
+    if ($type == 'id_soal')
+      return $this->db->delete('tb_analis_soalpg', ['id_soal' => $id]);
+  }
+}
