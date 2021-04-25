550b0dadf64cda5d0ded741e265236d05456bc12
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-25 14:20:28 +0700

    update func name

--- application/models/Sekolah_model.php
+++ application/models/Sekolah_model.php
@@ -3,27 +3,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Sekolah_model extends CI_Model
 {
-  // mengambil semua soal
-  public function getAllSoal()
+  // mengambil semua Sekolah
+  public function getAllSekolah()
   {
     return $this->db->get('tb_sekolah');
   }
 
-  // mengambil data soal berdasarkan tipe id
-  public function getSoalByid($id)
+  // mengambil data Sekolah berdasarkan tipe id
+  public function getSekolahByid($id)
   {
     // berdasarkan id sekolah
     return $this->db->get_where('tb_sekolah', ['id_sekolah' => $id]);
   }
 
-  // update soal dari id
-  public function upadateSoalById($id, $data)
+  // update Sekolah dari id
+  public function upadateSekolahById($id, $data)
   {
     return $this->db->update('tb_sekolah', $data, ['id_sekolah' => $id]);
   }
 
-  // delete soal
-  public function deleteSoalByType($id)
+  // delete Sekolah
+  public function deleteSekolahByType($id)
   {
     return $this->db->delete('tb_sekolah', ['id_sekolah' => $id]);
   }
