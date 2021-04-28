47803663ec1f7d7c6e507dde74ca7acaec1e5ea1
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-28 16:14:37 +0700

    update function in controller guru

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -17,25 +17,60 @@ class Guru extends CI_Controller
   public function index()
   {
     // dashboard
+    $data['tittle'] = 'Dashboard';
+    echo 'selamat datang guru';
+  }
+  public function analisis()
+  {
+    // read data analisis
+    $data['tittle'] = 'Hasil Analisis';
+  }
+  public function guruSiswa()
+  {
+    // Read Data Guru dan Siswa
+    $data['tittle'] = 'Daftar Guru dan Siswa';
+  }
+  public function mapel()
+  {
+    // Read Data Mapel
+    $data['tittle'] = 'Daftar Mata Pelajaran';
+  }
+  public function kelas()
+  {
+    // Read Data Kelas
+    $data['tittle'] = 'Daftar Kelas';
   }
   public function soal()
   {
-    // Input data soal
+    // Read & Input data soal
+    $data['tittle'] = 'Daftar Soal';
   }
   public function nilai()
   {
-    // Input Nilai per ujian siwa
+    // Read & Input Nilai per ujian siwa
   }
   public function skor()
   {
-    // Input skor soal
+    // Input skor soal Uraian
   }
   public function jawaban()
   {
-    // Input distribusi jawaban siswa
+    // Read & Input distribusi jawaban siswa dan kunci jawaban
+    $data['tittle'] = 'Distribusi Jawaban';
   }
-  public function kunciJawaban()
+  public function myProfile()
   {
-    // Input Kunci jawaban siswa
+    // read profile guru
+    $data['tittle'] = 'Profile Anda';
+  }
+  public function profileSekolah()
+  {
+    // read profile sekolah
+    $data['tittle'] = 'Profile Sekolah';
+  }
+  public function editProfile()
+  {
+    // edit profile guru
+    $data['tittle'] = 'Edit Profile Anda';
   }
 }
