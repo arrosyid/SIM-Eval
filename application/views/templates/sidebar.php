6ceb1dadc5a1ece0a9522ac88cc179a43e17ca45
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-04-28 16:14:10 +0700

    update sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,7 +2,7 @@
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
     <img src="<?= base_url() ?>assets/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">Argopuro Kurir</span>
+    <span class="brand-text font-weight-light">SIM Eval</span>
   </a>
 
   <!-- Sidebar -->
@@ -13,7 +13,7 @@
         <img src="<?= base_url() ?>assets/dist/img/user3-128x128.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Usaylatul Atiqah</a>
+        <a href="<?= base_url() ?>" class="d-block">Admin</a>
       </div>
     </div>
 
@@ -23,62 +23,77 @@
         <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
         <li class="nav-item has-treeview">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir') ?>" class="nav-link 
-                    <?php if ($title == 'Dashboard') echo 'active'; ?>">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru') ?>" class="nav-link 
+                    <?php if ($tittle == 'Dashboard') echo 'active'; ?>">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard<i class="right fas fa-angle-left"></i></p>
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir') ?>" class="nav-link">
+              <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/analisis') ?>" class="nav-link
+                        <?php if ($tittle == 'Hasil Analisis') echo 'active'; ?>">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Daftar Pengiriman</p>
+                <p>Hasil Analisis</p>
               </a>
             </li>
           </ul>
-          <!-- <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Daftar Pengiriman</p>
-              </a>
-            </li>
-          </ul> -->
         </li>
-        <li class="nav-header">RESI</li>
+        <li class="nav-header">Tabel</li>
         <li class="nav-item has-treeview">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir/pending') ?>" class="nav-link 
-                    <?php if ($title == 'pending') echo 'active'; ?>">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/guruSiswa') ?>" class="nav-link 
+                    <?php if ($tittle == 'Daftar Guru dan Siswa') echo 'active'; ?>">
             <i class="nav-icon far fa-clock"></i>
-            <p>pending</p>
+            <p>Daftar Guru dan Siswa</p>
           </a>
         </li>
         <li class="nav-item has-treeview">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir/sent') ?>" class="nav-link 
-                    <?php if ($title == 'Diterima Kantor') echo 'active'; ?>">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/mapel') ?>" class="nav-link 
+                    <?php if ($tittle == 'Daftar Mata Pelajaran') echo 'active'; ?>">
             <i class="nav-icon fas fa-truck-loading"></i>
-            <p>Diterima Kantor</p>
+            <p>Daftar Mapel</p>
           </a>
         </li>
         <li class="nav-item has-treeview">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir/process') ?>" class="nav-link 
-                    <?php if ($title == 'Dalam Proses') echo 'active'; ?>">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/kelas') ?>" class="nav-link 
+                    <?php if ($tittle == 'Daftar Kelas') echo 'active'; ?>">
             <i class="nav-icon fas fa-paper-plane"></i>
-            <p>Dalam Proses</p>
+            <p>Daftar Kelas</p>
           </a>
         </li>
         <li class="nav-item has-treeview">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('kurir/success') ?>" class="nav-link 
-                    <?php if ($title == 'Pengiriman Sukses') echo 'active'; ?>">
-            <i class="nav-icon fas fa-tasks"></i>
-            <p>Pengiriman Sukses</p>
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/soal') ?>" class="nav-link 
+                    <?php if ($tittle == 'Daftar Soal') echo 'active'; ?>">
+            <i class="nav-icon fas fa-paper-plane"></i>
+            <p>Daftar Soal</p>
+          </a>
+        </li>
+        <li class="nav-item has-treeview">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/nilai') ?>" class="nav-link 
+                    <?php if ($tittle == 'Distribusi Jawaban') echo 'active'; ?>">
+            <i class="nav-icon fas fa-paper-plane"></i>
+            <p>Distribusi Jawaban</p>
           </a>
         </li>
         <li class="nav-header">Setting</li>
         <li class="nav-item has-treeview">
-          <a href="#" class="nav-link">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/myProfile') ?>" class="nav-link
+                    <?php if ($tittle == 'Profile Anda') echo 'active'; ?>">
             <i class="nav-icon fas fa-id-card-alt"></i>
             <p>My Profile</p>
+          </a>
+        </li>
+        <li class="nav-item has-treeview">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/profileSekolah') ?>" class="nav-link
+                    <?php if ($tittle == 'Profile Sekolah') echo 'active'; ?>">
+            <i class="nav-icon fas fa-id-card-alt"></i>
+            <p>Profile sekolah</p>
+          </a>
+        </li>
+        <li class="nav-item has-treeview">
+          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/editProfile') ?>" class="nav-link
+                    <?php if ($tittle == 'Edit Profile Anda') echo 'active'; ?>">
+            <i class="nav-icon fas fa-id-card-alt"></i>
+            <p>Edit Profile</p>
           </a>
         </li>
         <li class="nav-item has-treeview">
