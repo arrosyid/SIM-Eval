291a49c299e415ccccb6324d6683e5542e1e8765
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-03 14:39:20 +0700

    +view tambah mapel

---
+++ application/views/admin/TambahMapel.php
@@ -0,0 +1,35 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="card card-primary">
+      <!-- /.card-header -->
+      <div class="card-header">
+        <h3 class="card-title">Tambahkan Mata Pelajaran</h3>
+      </div>
+      <div class="card-body">
+        <form role="form" action="" method="POST">
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Mata Pelajaran</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="mapel" id="mapel" placeholder="Isi Nama Mata Pelajaran" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              capthca
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">Tambah Mata Pelajaran</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</section>
