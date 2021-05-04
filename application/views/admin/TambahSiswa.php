7786d599bfc9b4cd9f10eb1878ba52f74e42c471
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-04 22:08:48 +0700

    +view Tambah Siswa

---
+++ application/views/admin/TambahSiswa.php
@@ -0,0 +1,95 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="card card-primary">
+      <!-- /.card-header -->
+      <div class="card-header">
+        <h3 class="card-title">Tambahkan Guru</h3>
+      </div>
+      <div class="card-body">
+        <form role="form" action="" method="POST">
+
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Jumlah Siswa yang ingin didaftarkan</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="loop" id="loop" placeholder="Isi Jumlah Yang ingin di Inputkan" value="">
+              </div>
+            </div>
+          </div>
+          <hr>
+
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Nama Siswa</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nm_siswa" id="nm_siswa" placeholder="Isi Nama Siswa" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>NIS</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nis" id="nis" placeholder="Isi Nomor Induk Siswa" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Kelas</label>
+            </div>
+            <div class="col-sm-5">
+              <div class="form-group">
+                <select class="form-control" name="id_kelas" id="id_kelas">
+                  <option value="">PILIH KELAS</option>
+                  <option value=""></option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Nama Siswa</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nm_siswa" id="nm_siswa" placeholder="Isi Nama Siswa" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>NIS</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nis" id="nis" placeholder="Isi Nomor Induk Siswa" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Kelas</label>
+            </div>
+            <div class="col-sm-5">
+              <div class="form-group">
+                <select class="form-control" name="id_kelas" id="id_kelas">
+                  <option value="">PILIH KELAS</option>
+                  <option value=""></option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</section>
