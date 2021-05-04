e6699b436f00f202e5db8a4e4da6458d4c880a05
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-04 22:09:43 +0700

    +view tambah Distribusi Jawaban uraian

---
+++ application/views/guru/TambahDistJwbUo.php
@@ -0,0 +1,64 @@
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Daftar Kooperasi</h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <a class="col-2 mb-4 btn btn-primary btn-block" href="#">Tambah Koperasi</a>
+            <table id="allPost" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th rowspan="2" class="align-middle">#</th>
+                  <th rowspan="2" class="align-middle">Nama siswa</th>
+                  <th colspan="4" class="text-center">Distribusi Nilai</th>
+                </tr>
+                <tr>
+                  <th>No.1</th>
+                  <th>No.2</th>
+                  <th>No.3</th>
+                  <th>No.4</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>nama
+                    <div>
+                      <a href="#" class="badge badge-success">edit</a>
+                      <a href="#" class="badge badge-danger">hapus</a>
+                      <a href="#" class="badge badge-warning">detail</a>
+                    </div>
+                  </td>
+                  <td>a
+                  </td>
+                  <td>b</td>
+                  <td>c</td>
+                  <td>d</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th>#</th>
+                  <th>Nama siswa</th>
+                  <th>No.1</th>
+                  <th>No.2</th>
+                  <th>No.3</th>
+                  <th>No.4</th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
