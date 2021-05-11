defb05b5d5f4de7ea5a4b533e8d2467ee46096c7
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-11 16:15:43 +0700

    +view ProfileAdmin

---
+++ application/views/admin/ProfileSekolah.php
@@ -0,0 +1,36 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="card card-primary">
+      <!-- /.card-header -->
+      <div class="card-header">
+        <h3 class="card-title">Profile Sekolah</h3>
+      </div>
+      <div class="card-body">
+
+        <strong><i class="fas fa-book mr-1"></i> Tentang kami</strong>
+        <ul class="text-muted">
+          <lu>Nama Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>NPSN : <a class="float-right">jsdak</a> <br></lu>
+          <lu>Nama Kepala Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Nama Admin Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Akreditasi : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Kurikulum : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Alamat : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Bentuk Pendidikan : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>SK Pendirian Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Tanggal SK Pendirian Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>SK Izin Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Tanggal SK Izin Sekolah : <a class="float-right">jsdakj</a> <br></lu>
+        </ul>
+        <hr>
+        <strong><i class="fas fa-address-book mr-1"></i>Contact Me</strong>
+        <ul class="text-muted">
+          <lu>Nomer Telfon : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Website : <a class="float-right">jsdakj</a> <br></lu>
+          <lu>Email : <a class="float-right">jsdakj</a> <br></lu>
+        </ul>
+      </div>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</section>
