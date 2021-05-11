c0ad126141cd739fbaf8f7802b77a4db8d66f65b
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-11 14:01:21 +0700

    +view editSekolah

---
+++ application/views/admin/EditSekolah.php
@@ -0,0 +1,206 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="card card-primary">
+      <!-- /.card-header -->
+      <div class="card-header">
+        <h3 class="card-title">Edit Sekolah</h3>
+      </div>
+      <div class="card-body">
+        <form role="form" action="" method="POST">
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Nama Sekolah</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nm_sekolah" id="nm_sekolah" placeholder="Isi Nama Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>NPSN</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="npsn" id="npsn" placeholder="Isi Nomor Pokok Sekolah Nasional" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Nama Kepala Sekolah</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nm_kepsek" id="nm_kepsek" placeholder="Isi Nama Kepala Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Nama Admin Sekolah</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="nm_admin" id="nm_admin" placeholder="Isi Nama Admin Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>No. Telfon</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="telfon" id="telfon" placeholder="Isi Nomor Telfon Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Website</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="website" id="website" placeholder="Isi Website Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Email</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="email" id="email" placeholder="Isi Email Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Akreditasi</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <select class="form-control" name="Akreditasi" id="Akreditasi">
+                  <option value="">PILIH AKREDITASI</option>
+                  <option value="A">A</option>
+                  <option value="B">B</option>
+                  <option value="C">C</option>
+                  <option value="">LAINNYA</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Kurikulum</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <select class="form-control" name="kurikulum" id="kurikulum">
+                  <option value="">PILIH KURIKULUM</option>
+                  <option value="K13">K13</option>
+                  <option value="KTSP">KTSP</option>
+                  <option value="2004">2004</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Alamat Lengkap</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Isi Alamat Lengkap" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Bentuk Pendidikan</label>
+            </div>
+            <div class="col-sm-5">
+              <div class="form-group">
+                <select class="form-control" name="bentuk_pendirikan" id="bentuk_pendirikan">
+                  <option value="">PILIH BENTUK PENDIDIKAN</option>
+                  <option value=""></option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>SK Pendirian Sekolah</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="sk_pendirian" id="sk_pendirian" placeholder="Isi SK Pendirian Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Tanggal SK Pendirian</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="date" class="form-control" name="tgl_sk_pendirian" id="tgl_sk_pendirian" placeholder="Isi Tanggal SK Pendirian Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>SK Izin Operasional</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="text" class="form-control" name="sk_izin" id="sk_izin" placeholder="Isi SK Izin Operasional Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Tanggal SK Izin Operasional</label>
+            </div>
+            <div class="col-sm-10">
+              <div class="form-group">
+                <input type="date" class="form-control" name="tgl_sk_izin" id="tgl_sk_izin" placeholder="Isi Tanggal SK Izin Operasional Sekolah" value="">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-2">
+              <label>Status </label>
+            </div>
+            <div class="col-sm-5">
+              <div class="form-group">
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="status" id="status" value="1">
+                  <label class="form-check-label">Aktif</label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="status" id="status" value="0">
+                  <label class="form-check-label">Tidak Aktif</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              capthca
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">Tambah Guru</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</section>
