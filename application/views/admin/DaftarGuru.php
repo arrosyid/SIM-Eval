0d4d5976d84ce3c7c78a015bd96665126daca1ba
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-14 13:09:45 +0700

    +view DaftarGuru

---
+++ application/views/admin/DaftarGuru.php
@@ -0,0 +1,65 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="card card-primary">
+      <!-- /.card-header -->
+      <div class="card-header">
+        <h3 class="card-title">Daftar Guru</h3>
+      </div>
+      <div class="card-body">
+        <table id="Table" class="table table-bordered table-striped">
+          <a href="#" class="col-2 mb-4 btn btn-primary btn-block">
+            Tambah Guru</a>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Guru</th>
+              <th>NIP</th>
+              <th>Mata Pelajaran</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mustiani</td>
+              <td>123761876</td>
+              <td>Matematika</td>
+              <td>
+                <a href="" data-toggle="modal" data-target="#editGuru" id="" class="badge badge-success view-data">edit</a>
+                <a href="#" class="badge badge-danger">hapus</a>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>No</th>
+              <th>Nama Guru</th>
+              <th>NIP</th>
+              <th>Mata Pelajaran</th>
+              <th>Action</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card-body -->
+
+    <!-- edit modal  -->
+    <div class="modal fade" id="editGuru" tabindex="-1" aria-labelledby="edit_GuruLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="edit_GuruLabel">Tambah Guru</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
