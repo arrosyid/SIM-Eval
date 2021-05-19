664f3fb8a03781e6e2cce59c23e223977ca20596
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-19 14:53:07 +0700

    +controller editProfile

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -113,7 +113,7 @@ class Guru extends CI_Controller
 
     $this->load->view('templates/admin_header', $data);
     $this->load->view('templates/sidebar', $data);
-    $this->load->view('admin/ProfileSekolah');
+    $this->load->view('guru/ProfileSekolah');
     $this->load->view('templates/admin_footer');
   }
 
@@ -121,5 +121,62 @@ class Guru extends CI_Controller
   {
     // edit profile guru
     $data['tittle'] = 'Edit Profile Anda';
+    $data['subtittle'] = 'Edit Profile Anda';
+    $data['user'] = $this->User_model->getUserByEmail($this->session->userdata['email']);
+    $data['guru'] = $this->Guru_model->getGuruByType('id_guru', $data['user']['id_guru']);
+
+    $this->form_validation->set_rules('nm_guru', 'Nama Guru', 'required|trim');
+    $this->form_validation->set_rules('nip', 'NIP', 'required|trim');
+    $this->form_validation->set_rules('id_mapel', 'Mata Pelajaran', 'required|trim');
+    $this->form_validation->set_rules('username', 'Username', 'required|trim');
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[tb_user.email]', [
+      'valid_email' => 'email tidak cocok',
+      'is_unique' => 'email sudah digunakan'
+    ]);
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/admin_header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('guru/EditProfile');
+      $this->load->view('templates/admin_footer');
+    } else {
+      $data_guru = [
+        'nm_guru' => htmlspecialchars($this->input->post('nm_guru', true)),
+        'nip' => htmlspecialchars($this->input->post('nip', true)),
+        'id_mapel' => htmlspecialchars($this->input->post('id_mapel', true))
+      ];
+      $data_akunGuru = [
+        'username' => htmlspecialchars($this->input->post('username', true)),
+        'email' => htmlspecialchars($this->input->post('email', true)),
+      ];
+      if ($this->db->insert('tb_akun', $data_akunGuru)) {
+        $this->session->set_flashdata(
+          'message1',
+          '<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                      Berhasil Mengubah Data Akun Anda</div>'
+        );
+      } else {
+        $this->session->set_flashdata(
+          'message1',
+          '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                      Gagal Mengubah Data Akun Anda</div>'
+        );
+      }
+      if ($this->db->insert('tb_guru', $data_guru)) {
+        $this->session->set_flashdata(
+          'message',
+          '<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                      Berhasil Mengubah Data Anda</div>'
+        );
+        redirect('admin/profileAdmin');
+      } else {
+        $this->session->set_flashdata(
+          'message',
+          '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                      Gagal Mengubah Data Anda</div>'
+        );
+        redirect('admin/profileAdmin');
+      }
+    }
   }
 }
