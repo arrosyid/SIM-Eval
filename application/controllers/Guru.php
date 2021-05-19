db83ee802ec6f63165bf86b5db0f9e2bcad09ea4
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-05-19 14:51:13 +0700

    +controller myProfile

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -59,11 +59,50 @@ class Guru extends CI_Controller
     // Read & Input distribusi jawaban siswa dan kunci jawaban
     $data['tittle'] = 'Distribusi Jawaban';
   }
+
   public function myProfile()
   {
     // read profile guru
     $data['tittle'] = 'Profile Anda';
+    $data['subtittle'] = 'Profile Anda';
+    $data['user'] = $this->User_model->getUserByEmail($this->session->userdata['email']);
+    $data['guru'] = $this->Guru_model->getGuruByType('id_guru', $data['user']['id_guru']);
+    // var_dump($data['guru']);
+    // die;
+    $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2]', [
+      'min_length' => 'password terlalu pendek!',
+      'matches' => 'password tidak sama!'
+    ]);
+    $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/admin_header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('guru/ProfileGuru');
+      $this->load->view('templates/admin_footer');
+    } else {
+      $data_password = [
+        'password' => password_hash(
+          htmlspecialchars($this->input->post('password1', true)),
+          PASSWORD_DEFAULT
+        )
+      ];
+      if ($this->User_model->updateUserById($data_password, $data['user']['id_user'])) {
+        $this->session->set_flashdata(
+          'message',
+          '<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        Berhasil Mengubah Password atau Kata Sandi Anda</div>'
+        );
+      } else {
+        $this->session->set_flashdata(
+          'message',
+          '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        Gagal Mengubah Password atau Kata Sandi Anda</div>'
+        );
+      }
+    }
   }
+
   public function profileSekolah()
   {
     // read profile sekolah
