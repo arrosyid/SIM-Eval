dcdc1467684aec811cd0087bbaebf2af738513c2
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-08-09 16:04:25 +0700

    update new sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,3 +1,15 @@
+<?php
+if ($user['level'] == 1) {
+  $href = base_url('admin/');
+  $role = 'Admin';
+} elseif ($user['level'] == 2) {
+  $href = base_url('guru/');
+  $role = 'Guru';
+} elseif ($user['level'] == 3) {
+  $href = base_url('siswa/');
+  $role = 'Siswa';
+}
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
@@ -13,7 +25,7 @@
         <img src="<?= base_url('assets/img/avatar.png') ?>" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru') ?>" class="d-block"><?= $user['level'] == 1 ? 'Admin' : 'Guru' ?></a>
+        <a href="<?= $href ?>" class="d-block"><?= $role ?></a>
       </div>
     </div>
 
@@ -23,93 +35,101 @@
         <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru') ?>" class="nav-link 
+          <a href="<?= $href ?>" class="nav-link 
                     <?php if ($tittle == 'dashboard') echo 'active'; ?>">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Dashboard</p>
           </a>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/analisis') : base_url('guru/analisis') ?>" class="nav-link
+          <a href="<?= $href . 'analisis' ?>" class="nav-link
                         <?php if ($tittle == 'Hasil Analisis') echo 'active'; ?>">
-            <i class="fas fa-chart-line nav-icon"></i>
+            <i class="nav-icon fas fa-chart-line"></i>
             <p>Hasil Analisis</p>
           </a>
         </li>
         </li>
-        <li class="nav-header">Tabel</li>
+        <li class="nav-header">Soal</li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarGuru') : base_url('guru/daftarGuru') ?>" class="nav-link 
+          <a href="<?= $href . 'daftarUjian' ?>" class="nav-link 
+          <?php if ($tittle == 'Daftar Ujian') echo 'active'; ?>">
+            <i class="nav-icon far fa-file-alt"></i>
+            <p>Daftar Ujian</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?= $href . 'daftarSoal' ?>" class="nav-link 
+                  <?php if ($tittle == 'Daftar Soal') echo 'active'; ?>">
+            <i class="nav-icon fas fa-lightbulb"></i>
+            <p>Daftar Soal</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?= $href . 'distJawaban' ?>" class="nav-link 
+                    <?php if ($tittle == 'Distribusi Jawaban' || $tittle == 'Koreksi Soal') echo 'active'; ?>">
+            <i class="nav-icon fas fa-spell-check"></i>
+            <p>Distribusi Jawaban</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?= $href . 'nilai' ?>" class="nav-link 
+                    <?php if ($tittle == 'Daftar Nilai') echo 'active'; ?>">
+            <i class="nav-icon fas fa-poll-h"></i>
+            <p>Nilai Siswa</p>
+          </a>
+        </li>
+        <li class="nav-header">Daftar</li>
+        <li class="nav-item">
+          <a href="<?= $href . 'daftarGuru' ?>" class="nav-link 
                     <?php if ($tittle == 'Daftar Guru') echo 'active'; ?>">
             <i class="nav-icon fas fa-address-book"></i>
             <p>Daftar Guru</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarSiswa') : base_url('guru/daftarSiswa') ?>" class="nav-link 
+          <a href="<?= $href . 'daftarSiswa' ?>" class="nav-link 
                     <?php if ($tittle == 'Daftar Siswa') echo 'active'; ?>">
             <i class="nav-icon fas fa-user-graduate"></i>
             <p>Daftar Siswa</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarMapel') : base_url('guru/daftarMapel') ?>" class="nav-link 
+          <a href="<?= $href . 'daftarMapel' ?>" class="nav-link 
                     <?php if ($tittle == 'Daftar Mapel') echo 'active'; ?>">
             <i class="nav-icon fas fa-book"></i>
             <p>Daftar Mapel</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarKelas') : base_url('guru/daftarKelas') ?>" class="nav-link 
+          <a href="<?= $href . 'daftarKelas' ?>" class="nav-link 
                     <?php if ($tittle == 'Daftar Kelas') echo 'active'; ?>">
             <i class="nav-icon fas fa-chalkboard-teacher"></i>
             <p>Daftar Kelas</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarSoal') : base_url('guru/daftarSoal') ?>" class="nav-link 
-                    <?php if ($tittle == 'Daftar Soal') echo 'active'; ?>">
-            <i class="nav-icon fas fa-lightbulb"></i>
-            <p>Daftar Soal</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarUjian') : base_url('guru/daftarUjian') ?>" class="nav-link 
-                    <?php if ($tittle == 'Daftar Ujian') echo 'active'; ?>">
-            <i class="nav-icon far fa-file-alt"></i>
-            <p>Daftar Ujian</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/daftarPelajaran') : base_url('guru/daftarPelajaran') ?>" class="nav-link 
+          <a href="<?= $href . 'daftarPelajaran' ?>" class="nav-link 
                     <?php if ($tittle == 'Daftar Pelajaran') echo 'active'; ?>">
             <i class="nav-icon fas fa-clipboard-list"></i>
             <p>Daftar Pelajaran</p>
           </a>
         </li>
-        <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin') : base_url('guru/nilai') ?>" class="nav-link 
-                    <?php if ($tittle == 'Distribusi Jawaban') echo 'active'; ?>">
-            <i class="nav-icon fas fa-spell-check"></i>
-            <p>Distribusi Jawaban</p>
-          </a>
-        </li>
         <li class="nav-header">Setting</li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/profileAdmin') : base_url('guru/myProfile') ?>" class="nav-link
+          <a href="<?= $href . 'profileAdmin' ?>" class="nav-link
                     <?php if ($tittle == 'Profile Anda' || $tittle == 'Profile Admin') echo 'active'; ?>">
             <i class="nav-icon fas fa-address-card"></i>
             <p>My Profile</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/profileSekolah') : base_url('guru/profileSekolah') ?>" class="nav-link
+          <a href="<?= $href . 'profileSekolah' ?>" class="nav-link
                     <?php if ($tittle == 'Profile Sekolah') echo 'active'; ?>">
             <i class="nav-icon fas fa-school"></i>
             <p>Profile sekolah</p>
           </a>
         </li>
         <li class="nav-item">
-          <a href="<?= $user['level'] == 1 ? base_url('admin/editProfile') : base_url('guru/editProfile') ?>" class="nav-link
+          <a href="<?= $href . 'editProfile' ?>" class="nav-link
                     <?php if ($tittle == 'Edit Profile Anda') echo 'active'; ?>">
             <i class="nav-icon fas fa-user-edit"></i>
             <p>Edit Profile</p>
@@ -125,13 +145,11 @@
           </li>
         <?php } ?>
         <li class="nav-item">
-          <a href="<?= base_url() ?>auth/logout" class="nav-link">
+          <a href="<?= base_url('auth/logout') ?>" class="nav-link">
             <i class="nav-icon fas fa-sign-out-alt"></i>
             <p>Keluar</p>
           </a>
         </li>
-      </ul>
-      </li>
       </ul>
     </nav>
   </div>
