f5653ad21b632723f9bba2f761f2fc29a2052491
arrosyid <iqbal.rosyidi.32@gmail.com>
2021-08-17 14:22:16 +0700

    new type in getJawabanByType

--- application/models/Jawaban_model.php
+++ application/models/Jawaban_model.php
@@ -3,69 +3,87 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Jawaban_model extends CI_Model
 {
-  // mengambil semua jawab
-  public function getAllJawab()
+  // mengambil semua Jawaban
+  public function getAllJawaban()
   {
-    $this->db->select('tb_dis_jwb.*, tb_siswa.*, tb_ujian.*')
-      ->from('tb_dis_jwb')
-      ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dis_jwb.id_siswa')
-      ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dis_jwb.id_ujian');
+    $this->db->select('tb_dist_jwb.*, tb_siswa.*, tb_ujian.*')
+      ->from('tb_dist_jwb')
+      ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dist_jwb.id_siswa')
+      ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dist_jwb.id_ujian');
     return $this->db->get()->result_array();
   }
 
-  // mengambil data jawab berdasarkan tipe id
-  public function getJawabByType($type, $id)
+  // mengambil data Jawaban berdasarkan tipe id
+  public function getJawabanByType($type, $id, $id_siswa = null)
   {
     // berdasarkan idjawab
     if ($type == 'id_jawab') {
-      $this->db->select('tb_dis_jwb.*, tb_siswa.*, tb_ujian.*')
-        ->from('tb_dis_jwb')
+      $this->db->select('tb_dist_jwb.*, tb_siswa.*, tb_ujian.*')
+        ->from('tb_dist_jwb')
         ->where(['id_jawab' => $id])
-        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dis_jwb.id_siswa')
-        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dis_jwb.id_ujian');
+        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dist_jwb.id_siswa')
+        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dist_jwb.id_ujian');
       return $this->db->get()->row_array();
     }
 
     // berdasarkan id_siswa
     if ($type == 'id_siswa') {
-      $this->db->select('tb_dis_jwb.*, tb_siswa.*, tb_ujian.*')
-        ->from('tb_dis_jwb')
+      $this->db->select('tb_dist_jwb.*, tb_siswa.*, tb_ujian.*')
+        ->from('tb_dist_jwb')
         ->where(['tb_siswa.id_siswa' => $id])
-        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dis_jwb.id_siswa')
-        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dis_jwb.id_ujian');
+        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dist_jwb.id_siswa')
+        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dist_jwb.id_ujian');
       return $this->db->get()->result_array();
     }
 
     // berdasarkan id_ujian
     if ($type == 'id_ujian') {
-      $this->db->select('tb_dis_jwb.*, tb_siswa.*, tb_ujian.*')
-        ->from('tb_dis_jwb')
+      $this->db->select('tb_dist_jwb.*, tb_siswa.*, tb_ujian.*')
+        ->from('tb_dist_jwb')
         ->where(['tb_ujian.id_ujian' => $id])
-        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dis_jwb.id_siswa')
-        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dis_jwb.id_ujian');
+        ->join('tb_siswa', 'tb_siswa.id_siswa = tb_dist_jwb.id_siswa')
+        ->join('tb_ujian', 'tb_ujian.id_ujian = tb_dist_jwb.id_ujian');
       return $this->db->get()->result_array();
+    }
+    // berdasarkan id_ujian, id_siswa dan pilihan ganda
+    if ($type == 'id_ujian_siswa_pg') {
+      $this->db->select('tb_dist_jwb.*')
+        ->from('tb_dist_jwb')
+        ->where(['tb_dist_jwb.id_ujian' => $id])
+        ->where(['tb_dist_jwb.id_siswa' => $id_siswa])
+        ->where(['tb_dist_jwb.jenis_soal' => 'PILIHAN GANDA']);
+      return $this->db->get()->row_array();
+    }
+    // berdasarkan id_ujian, id_siswa dan URAIAN
+    if ($type == 'id_ujian_siswa_uo') {
+      $this->db->select('tb_dist_jwb.*')
+        ->from('tb_dist_jwb')
+        ->where(['tb_dist_jwb.id_ujian' => $id])
+        ->where(['tb_dist_jwb.id_siswa' => $id_siswa])
+        ->where(['tb_dist_jwb.jenis_soal' => 'URAIAN']);
+      return $this->db->get()->row_array();
     }
   }
 
-  // update jawab dari id
-  public function upadateJawabById($id, $data)
+  // update jawaban dari id
+  public function upadateJawabanById($id, $data)
   {
-    return $this->db->update('tb_dis_jwb', $data, ['id_jawab' => $id]);
+    return $this->db->update('tb_dist_jwb', $data, ['id_jawab' => $id]);
   }
 
-  // delete jawab
-  public function deleteJawabByType($type, $id)
+  // delete jawaban
+  public function deleteJawabanByType($type, $id)
   {
     // berdasarkan id jawab
     if ($type == 'id_jawab')
-      return $this->db->delete('tb_dis_jwb', ['id_jawab' => $id]);
+      return $this->db->delete('tb_dist_jwb', ['id_jawab' => $id]);
 
     // berdasarkan id_siswa
     if ($type == 'id_siswa')
-      return $this->db->delete('tb_dis_jwb', ['id_siswa' => $id]);
+      return $this->db->delete('tb_dist_jwb', ['id_siswa' => $id]);
 
     // berdasarkan id_ujian
     if ($type == 'id_ujian')
-      return $this->db->delete('tb_dis_jwb', ['id_ujian' => $id]);
+      return $this->db->delete('tb_dist_jwb', ['id_ujian' => $id]);
   }
 }
